be7b90f321cea2199a52eff3a90f6cd246e45a51
Studentdot0600 <studentdot0600@de.techni.dom>
2022-10-14 16:44:30 +0200

    Step 03

--- Exo_ASP_01.App/Controllers/GameController.cs
+++ Exo_ASP_01.App/Controllers/GameController.cs
@@ -49,7 +49,7 @@ namespace Exo_ASP_02.App.Controllers
         }
 
         [HttpPost]
-        public IActionResult Add(GameAdd game)
+        public IActionResult Add(GameAdd game, IFormFile image)
         {
             if(!ModelState.IsValid)
             {
@@ -58,7 +58,11 @@ namespace Exo_ASP_02.App.Controllers
                 return View();
             }
 
+            // Sauvegarde du fichier
+            string filename = saveImageInRoot(image);
 
+            // Conversion de la liste de genre (string) en GameGenre
+            // - v1
             List<GameGenre> genres = new List<GameGenre>();
             foreach(string? genre in game.Genres)
             {
@@ -70,12 +74,13 @@ namespace Exo_ASP_02.App.Controllers
                 }
             }
 
+            // - v2
             List<GameGenre> genres2 = game.Genres
                 .Where(g => g != null)
                 .Select(g => FakeDB.GetOrInsertGenre(g))
                 .ToList();
 
-
+            // Ajout dans la FakeDB
             FakeDB.InsertGame(new Game()
             {
                 Name = game.Name,
@@ -83,10 +88,36 @@ namespace Exo_ASP_02.App.Controllers
                 ReleaseYear = game.ReleaseYear,
                 Price = (double)game.Price,
                 PEGI = game.PEGI,
-                Genres = genres
+                Genres = genres,
+                ImgSrc = filename
             });
 
             return RedirectToAction("Index");
+        }
+
+        private string saveImageInRoot(IFormFile image)
+        {
+            // Définition du répertoire pour sauvegarder les images
+            string directory = Path.Combine(Environment.CurrentDirectory, "wwwroot", "images");
+
+            // Définition du nom de fichier 
+            string now = DateTime.UtcNow.ToString("yyyyMMdd-HHmmss");
+            string rng = Guid.NewGuid().ToString();
+            string ext = Path.GetExtension(image.FileName);
+            string filename = now + "_" + rng + ext;
+
+            // Création du chemin d'acces au fichier
+            string file = Path.Combine(directory, filename);
+
+            // Ouverture d'un flux d'ecriture pour le fichier
+            using (FileStream fs = new FileStream(file, FileMode.CreateNew))
+            {
+                // Sauvegarder l'image via le flux
+                image.CopyTo(fs);
+            }
+
+            // Renvoi du nom du fichier
+            return "/images/" + filename;
         }
     }
 }
